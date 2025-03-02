789857f452e91939137cfbac18b9f6f916d18a68
K V ABHIJITH <43536678+abhijithkv056@users.noreply.github.com>
2025-03-02 19:11:47 +0530

    title modified

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.set_page_config(
 
 
 # --- Header ---
-st.html("""<h2 style="text-align: center;">RAG project""")
+st.html("""<h2 style="text-align: center;">RAG project- V2""")
 
 
 # --- Initial Setup ---
