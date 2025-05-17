f606a38e350a43316b5de38071c0b21a8c613e38
Abhijith <abhijithkv56@gmail.com>
2025-05-17 14:57:51 +0530

    pysqlite removed from main

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ import uuid
 import os
 #os.system("pip install -r requirements.txt")
 
-# check if it's linux so it works on Streamlit Cloud
-if os.name == 'posix':
-    __import__('pysqlite3')
-    import sys
-    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from langchain_openai import ChatOpenAI, AzureChatOpenAI
 from langchain_anthropic import ChatAnthropic
